154649d3fdfdee9d20ef8346ce0a154b62ed0ad5
A-Green2 <ambrg@umich.edu>
2024-07-11 13:34:20 -0400

    wip

--- src/denoiseImage.py
+++ src/denoiseImage.py
@@ -3,6 +3,20 @@ import numpy as np
 import random
 import math
 
+# function to evaluate the likelihood of a pixel's color
+def calculate_phi(actual_pixel_value, expected_pixel_value, sigma):
+    numerator = -1 * math.pow((actual_pixel_value - expected_pixel_value))
+    x = math.exp(numerator / math.pow(sigma))
+    return x
+
+#  RGB color list
+RGB_colors = [
+    [225, 0, 0],
+    [0, 225, 0],
+    [0, 0, 225]
+]
+
+
 # open image
 with Image.open('noisy_rgb_image.png') as im:
     
@@ -13,7 +27,11 @@ with Image.open('noisy_rgb_image.png') as im:
 width = im.width
 height = im.height
 
+mean_deviation = 5
+
 # iterate through each pixel
 for w in range(width):
     for h in range(height):
-        # calculate PHI
+        
+        # calculate phi
+        phi = calculate_phi(pixel[w, h], RGB_colors, mean_deviation)
