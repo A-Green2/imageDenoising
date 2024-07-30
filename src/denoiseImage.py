75440f2d3ce833236518a64a168a850012e8023e
A-Green2 <ambrg@umich.edu>
2024-07-30 12:07:20 -0400

    added new image and fixed phi_value_list

--- src/denoiseImage.py
+++ src/denoiseImage.py
@@ -4,6 +4,19 @@ import random
 import math
 
 # TODO: create another data structure thats image height and width * 3 -> 3 because RGB are the classes
+# store phi values for each pixel with respect to each class
+# phi = what I think I should be based off what I see
+# message = what my neighbor thinks I should be based off what they see
+def new_image(original_image_width, original_image_height):
+    new_image_width = original_image_width * 3
+    new_image_height = original_image_height * 3
+    # new_image = new_image_width * new_image_height
+    
+    # array for the image with a white background
+    image_array = np.ones((new_image_height, new_image_width, 3), dtype=np.uint8) * 255
+
+    return image_array
+
 
 # function to evaluate the likelihood of a pixel's color
 # evaluate phi for all 3 colors store in data structure above - take the best/highest value 
@@ -13,8 +26,16 @@ def calculate_phi(actual_pixel_value, expected_pixel_value, sigma):
     # TODO: call function using example values to check function
     # numerator = -1 * math.pow((actual_pixel_value - expected_pixel_value), 2)
     pixel_diff = ()
-    pixel_diff = (actual_pixel_value[0] - expected_pixel_value[0], actual_pixel_value[1] - expected_pixel_value[1], actual_pixel_value[2] - expected_pixel_value[2])
-    numerator = -1 * (math.pow(pixel_diff[0], 2) + math.pow(pixel_diff[1], 2) + math.pow(pixel_diff[2], 2))
+    pixel_diff = (actual_pixel_value[0] - expected_pixel_value[0], 
+                  actual_pixel_value[1] - expected_pixel_value[1], 
+                  actual_pixel_value[2] - expected_pixel_value[2]
+                  )
+    
+    numerator = -1 * (math.pow(pixel_diff[0], 2) + 
+                      math.pow(pixel_diff[1], 2) + 
+                      math.pow(pixel_diff[2], 2)
+                      )
+    
     x = math.exp(numerator / math.pow(sigma, 2))
     return x
 
@@ -30,18 +51,27 @@ with Image.open('noisy_rgb_image.png') as im:
     
     # access individual pixel values - list datatype
     pixel = im.load()
-    
-print(tuple(pixel[0, 0]))
 
 # get width and height of image
 width = im.width
 height = im.height
 
+# initialize the variance, sigma
 mean_deviation = 5
 
+# initialize list to store phi values
+# phi_values_list = []
+# initialize array storing phi values for each pixel's RGB values
+phi_values_list = np.zeros((width, height, 3))
+
 # iterate through each pixel
+# print(width)
 for w in range(width):
+    # print("w: ", w)
+    # phi_values_list[w] = []
+    
     for h in range(height):
+        # phi_values_list[w][h]= []
         
         # iterate over colors
         for i in range(len(RGB_colors)):
@@ -49,9 +79,16 @@ for w in range(width):
             # convert pixel list to pixel tuple
             pixel[w, h] = tuple(pixel[w, h])
             
-            print("pixel: ", pixel[w, h])
+            # print("pixel: ", pixel[w, h])
             
-            # calculate phi
-            phi = ()
+            # calculate phi 
             phi = calculate_phi(pixel[w, h], RGB_colors[i], mean_deviation)
-            
+            # print("phi: ", phi)
+            # print(f"phi for pixel ({w}, {h}) and color {RGB_colors[i]}: {phi}")
+            # TODO: save as list then convert to tuple
+            phi_values_list[w][h][i] = phi
+
+print("phi list: ", phi_values_list[0][0][0])
+
+# # iterate over the phi of each pixel
+# for p in phi_values_list:
